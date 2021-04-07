c10a294c5d08fc570f74625e9fdfe6585ca6f186
Anastasia <astrarta1378@bk.ru>
2021-04-07 23:55:53 +0300

    Add Youtube Site header

---
+++ REACT/try1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <ul>
+                <li>Nice try</li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
